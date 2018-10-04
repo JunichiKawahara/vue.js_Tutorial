6056c3d7a7e923136e9da72b17b69fc36ad54ace
JunichiKawahara <kawahara-j@hi-ad.jp>
2018-10-04 14:08:48 +0900

    section14

---
+++ section14/main.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello!',
+        val: true,
+        arrayVal: [],
+        radioVal: '',
+        selectVal: 'default',
+        multiSelectVal: []
+
+    }
+})
