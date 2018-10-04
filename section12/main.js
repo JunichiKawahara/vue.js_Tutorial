653e59b70b8c0f77960d6dba753a3a8b8bb1c248
JunichiKawahara <kawahara-j@hi-ad.jp>
2018-10-04 10:13:57 +0900

    Section 12

--- section12/main.js
+++ section12/main.js
@@ -1,3 +1,8 @@
 var app = new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+        url: 'http://www.google.co.jp',
+        message: 'Vue.js',
+        htmlMessage: '<strong>Vuew.js</strong>'
+    }
 })
